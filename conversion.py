b294ea3b5680c489deca70ba0be3e2f4b0b5609a
Mingshi Chen <114925342+Mancy-Chen@users.noreply.github.com>
2023-06-20 12:12:32 +0200

    Add new function
    celsius to kelvin

--- conversion.py
+++ conversion.py
@@ -2,3 +2,7 @@ def celsius_to_fahrenheit(celsius):
     fahrenheit = celsius/5*9 + 32
     return fahrenheit
     
+# Mancy's Version
+def celsius_to_kelvin(celsius):
+    kelvin = celsius + 273.15
+    return kelvin
